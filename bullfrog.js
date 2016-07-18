fdfab66ed07ee5bd5808ec5c926830acb5f15c0a
roylaurie <roy.laurie@asimovian.software>
2016-07-18 15:21:05 -0700

    wip

--- bullfrog.js
+++ bullfrog.js
@@ -3,9 +3,11 @@
 		baseUrl: 'js'	
 	};
 
-	requirejs(['bullfrog/Service'], function(BullfrogService) {
-		var bullfrogService = new BullfrogService();
-		bullfrogService.start();
+	requirejs(['bullfrog/Service', 'bullfrog/Config'], function(BullfrogService, Config) {
+		Config.load(function(config) {
+			var bullfrogService = new BullfrogService(config);
+			bullfrogService.start();
+		});
 	});
 
 })();
